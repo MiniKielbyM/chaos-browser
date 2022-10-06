554ecb17658b34b8155371876d0549d928f2d107
MiniKielbyM <85803354+MiniKielbyM@users.noreply.github.com>
2022-10-06 10:24:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const baseGoogle = "https://www.google.com/search?q=";
+const google = "https://www.google.com/search?q=";
 const q = document.getElementById('search');
 
 function submitted() {
-  const url = google + site + '+' + q.value;
-  const win = window.open(url, '_blank');
-  win.focus();
+  const url = google + q.value;
+  window.open(url, '_blank');
 }
