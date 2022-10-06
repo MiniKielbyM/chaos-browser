39264f3149e245b8666164fcb221f40351de928d
MiniKielbyM <85803354+MiniKielbyM@users.noreply.github.com>
2022-10-06 07:49:11 -0400

    did moar stuff

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//https://www.google.com/search?q=//
+const baseGoogle = "https://www.google.com/search?q=";
+const q = document.getElementById('search');
+
+function submitted() {
+  const url = google + site + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
