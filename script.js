dec863ca462e49738f5cc21ce910112c17053446
MiniKielbyM <85803354+MiniKielbyM@users.noreply.github.com>
2022-10-07 08:35:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 const google = "https://www.google.com/search?q=";
 const q = document.getElementById('search');
-
-function submitted() {
-  const url = google + q.value;
-  window.open(url, '_blank');
-}
-const exampleBtn = document.getElementByClassName('btn');
-
-exampleBtn.addEventListener('click', () => {
-  window.open('https://example.com', '_blank');
-});
